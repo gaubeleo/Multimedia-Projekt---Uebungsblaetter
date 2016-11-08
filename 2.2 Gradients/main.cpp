84dc4e52b19c5b91dbb62bc14b4b8ae2f05c7d10
gaubeleo <laxleo@outlook.de>
2016-11-08 19:20:50 +0100

    weird memory bug!!

--- 2.2 Gradients/main.cpp
+++ 2.2 Gradients/main.cpp
@@ -80,7 +80,7 @@ Mat filter(const Mat &img, const Mat &kernel, bool normalize){
 	for (int y = 0; y < img.rows; y++){
 		//copy border from original Image
 		if (y < yOffset || y >= img.rows - yOffset){
-			img.row(y).copyTo(filteredImg.row(y));
+			
 			continue;
 		}
 
@@ -117,21 +117,25 @@ Mat calcMagnitude(const Mat &X, const Mat &Y){
 	assert(X.size() == Y.size());
 	assert(X.type() == CV_16SC1 && Y.type() == CV_16SC1);
 
-	Mat mag(X.rows, X.cols, CV_8UC1);
+	Mat mag(X.rows, X.cols, CV_16SC1);
 
 	for (int y = 0; y < mag.rows; y++){
-		uchar *row = mag.ptr<uchar>(y);
+		short *row = mag.ptr<short>(y);
 		const short *rowX = X.ptr<short>(y);
 		const short *rowY = Y.ptr<short>(y);
 		for (int x = 0; x < mag.cols; x++){
-			int value = 0.5*abs(rowX[x]) + 0.5*abs(rowY[x]);
-			row[x] = (uchar) min(255, value);
+			// magnitude calculation to match OpenCV, but for displayable Magnitude-Image: don't divide normValue by 4 to achieve same result as OpenCV
+			//int value = 0.5*min(255, abs(rowX[x])) + 0.5*min(255, abs(rowY[x]));
+			// magnitude calculation using Script
+			int value = abs(rowX[x]) + abs(rowY[x]);
+			// should not exceed max value of short, but just to be sure:
+			row[x] = (short) min(32768, value);
 		}
 	}
 	return mag;
 }
 
-Mat convertToImg(const Mat &img){
+double getAbsMax(const Mat &img){
 	assert(img.type() == CV_16SC1);
 
 	double normValue = 1.;
@@ -142,26 +146,88 @@ Mat convertToImg(const Mat &img){
 				normValue = abs(row[x]);
 		}
 	}
+	assert(normValue != 0);
+	return normValue;
+}
+
+Mat convertToImg(const Mat &img){
+	assert(img.type() == CV_16SC1);
+
+	double normValue = getAbsMax(img);
+	// normValue is the absolute maximal value in the original Sobel-Mat
+	// however OpenCV seems to be using a fourth of that value to normalize its own Sobel Image Representation
+	normValue /= 4;
 
 	Mat convertedImg(img.rows, img.cols, CV_8UC1);
 	for (int y = 0; y < img.rows; y++){
 		uchar *row = convertedImg.ptr<uchar>(y);
 		const short *rowOrg = img.ptr<short>(y);
 		for (int x = 0; x < img.cols; x++){
-			row[x] = (uchar) abs(((double)rowOrg[x]/normValue)*255.);
+			row[x] = (uchar) min(255., abs(((double)rowOrg[x]/normValue)*255.));
 		}
 	}
 	return convertedImg;
 }
 
-//Mat calcGradientDirection(const Mat &mag){
-//	//
-//}
+Mat calcGradients(const Mat &X, const Mat &Y){
+	assert(X.size() == Y.size());
+	assert(X.type() == CV_16SC1 && Y.type() == CV_16SC1);
+
+	Mat gradients(X.rows, X.cols, CV_32FC1);
+
+	for (int y = 0; y < gradients.rows; y++){
+		double *row = gradients.ptr<double>(y);
+		const short *rowX = X.ptr<short>(y);
+		const short *rowY = Y.ptr<short>(y);
+		for (int x = 0; x < gradients.cols; x++){
+			double direction = atan2(rowY[x], rowX[x]);
+			row[x] = direction;
+		}
+	}
+	return gradients;
+}
+
+void _drawGradient(Mat &gradImg, int x, int y, double gradDir, short gradMag){
+	double length = 0.05;
+	int xOffset = (gradMag * cos(gradDir) * length)/2;
+	int yOffset = (gradMag * sin(gradDir) * length)/2;
+
+	Point start = Point(x - xOffset, y - yOffset);
+	Point end = Point(x + xOffset, y + yOffset);
+
+	line(gradImg, start, end, Scalar(0, 255, 0));
+	if (gradDir < 0 && yOffset > 0)
+		circle(gradImg, start, 2, Scalar(0, 255, 0), -1);
+	else
+		circle(gradImg, end, 2, Scalar(0, 255, 0), -1);
+	circle(gradImg, Point(x, y), 2, Scalar(0, 0, 255), -1);
+}
+
+Mat drawGradients(const Mat &img, const Mat &gradients, const Mat &dervMag){
+	assert(img.channels() == 3);
+	assert(img.type() == CV_8UC3 && gradients.type() == CV_32FC1 && dervMag.type() == CV_16SC1);
+	assert(img.size() == gradients.size() && img.size() == dervMag.size());
+
+	short threshold = 150;
+
+	Mat gradImg = img.clone();
+	for (int y = 0; y < img.rows; y+=5){
+		const double *rowGrad = gradients.ptr<double>(y);
+		const short *rowMag = dervMag.ptr<short>(y);
+		for (int x = 0; x < img.cols; x+=5){
+			if (rowMag[x] > threshold){
+				_drawGradient(gradImg, x, y, rowGrad[x], rowMag[x]);
+			}
+		}
+	}
+	return gradImg;
+}
 
 /////////////////////////////////////////////////////////////////////////////
 
 int main(){
 	Mat img = loadImg("src", "Testimage_gradients.jpg", IMREAD_GRAYSCALE); //IMREAD_COLOR
+	//Mat img = loadImg("src", "lenna.jpg", IMREAD_GRAYSCALE); //IMREAD_COLOR
 
 	// Aufgabe a)
 	Mat dervX = sobelX(img);
@@ -171,30 +237,44 @@ int main(){
 
 	// Aufgabe b)
 	Mat dervMag = calcMagnitude(dervX, dervY);
+	Mat dervImgMag = convertToImg(dervMag);
 
 	// Aufgabe c)
-	//Mat gradientDir = calcGradientDirection(mag);
+	Mat gradients = calcGradients(dervX, dervY);
 
-	Mat grad_x, grad_y, Xcv, Ycv, Magcv;
+	//Aufgabe d)
+	Mat colorImg(img.rows, img.cols, CV_8UC3);
+	cvtColor(img, colorImg, CV_GRAY2RGB);
 
-	Sobel(img, grad_x, CV_16SC1, 1, 0, 3, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(grad_x, Xcv);
+	Mat gradImg = drawGradients(colorImg, gradients, dervMag);
 
-	/// Gradient Y
-	//Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-	Sobel(img, grad_y, CV_16SC1, 0, 1, 3, 1, 0, BORDER_DEFAULT);
-	convertScaleAbs(grad_y, Ycv);
+	// displaying and saving images
 
-	/// Total Gradient (approximate)
-	addWeighted(Xcv, 0.5, Ycv, 0.5, 0, Magcv);
+	//imshow("Sobel X", dervImgX);
+	//imshow("Sobel Y", dervImgY);
+	//imshow("Magnitude", dervImgMag);
+	imshow("Gradient Image", gradImg);
 
-	imshow("Sobel X", dervImgX);
-	imshow("Sobel Y", dervImgY);
-	imshow("Magnitude", dervMag);
+	saveImg("results", "Sobel X.jpg", dervImgX);
+	saveImg("results", "Sobel Y.jpg", dervImgY);
+	saveImg("results", "Magnitude.jpg", dervImgMag);
+	saveImg("results", "Gradient Image.jpg", gradImg);
 
-	imshow("Sobel X CV", Xcv);
-	imshow("Sobel Y CV", Ycv);
-	imshow("Magnitude CV", Magcv);
+	//// for OpenCV comparison:
+	//Mat grad_x, grad_y, Xcv, Ycv, Magcv;
+
+	//Sobel(img, grad_x, CV_16SC1, 1, 0, 3, 1, 0, BORDER_DEFAULT);
+	//convertScaleAbs(grad_x, Xcv);
+
+	//Sobel(img, grad_y, CV_16SC1, 0, 1, 3, 1, 0, BORDER_DEFAULT);
+	//convertScaleAbs(grad_y, Ycv);
+
+	//addWeighted(Xcv, 0.5, Ycv, 0.5, 0, Magcv);
+
+	//imshow("Sobel X CV", Xcv);
+	//imshow("Sobel Y CV", Ycv);
+	//imshow("Magnitude CV", Magcv);
+	//// end OpenCV
 
 	waitKey();
 	destroyAllWindows();
